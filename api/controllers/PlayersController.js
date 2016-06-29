c21d39b9c1608612ebda17d4645543bca876e69d
Zeb Burke-Conte <zmbc@uw.edu>
2016-06-29 16:25:29 -0400

    Make player view slightly nicer

--- api/controllers/PlayersController.js
+++ api/controllers/PlayersController.js
@@ -1,21 +1,23 @@
 /**
- * PlayerController
+ * PlayersController
  *
  * @description :: Server-side logic for managing Players
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
 module.exports = {
-	view: function(req, res) {		
-		Players.findOne(req.params.id)
-			.populate('team')
-			.exec(function(err, playerRecord) {
-				if (typeof playerRecord === 'undefined' || playerRecord === null) {
-					res.notFound();
-				} else {
-					res.view('view.ejs', {player: playerRecord});
-				}
-			});
-	}
+  view: function(req, res) {
+    Players.findOne(req.params.id)
+      .populate('team')
+      .populate('performances')
+      .exec(function(err, playerRecord) {
+        if (typeof playerRecord === 'undefined' || playerRecord === null) {
+          res.notFound();
+        } else {
+          playerRecord.performances = playerRecord.performances.slice(0, 10);
+          res.view('players/view.ejs', {player: playerRecord});
+        }
+      });
+  }
 };
 
