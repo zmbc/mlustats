b863ad19a6fac8de3678a442433ad876a0ef4eab
zebburkeconte <zebburkeconte@gmail.com>
2016-07-18 23:28:37 -0700

    Refactor controller to use promises

--- api/controllers/PlayersController.js
+++ api/controllers/PlayersController.js
@@ -5,19 +5,30 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+ var Promise = require('bluebird');
+
 module.exports = {
   view: function(req, res) {
-    Players.findOne(req.params.id)
-      .populate('team')
-      .exec(function(err, playerRecord) {
-        if (typeof playerRecord === 'undefined' || playerRecord === null) {
-          res.notFound();
-        } else {
-          Statistics.findOne({player: playerRecord.id, week: null, season: 1, team: null}).exec(function (err, statsRecord) {
-            res.view('players/view.jade', {player: playerRecord, stats: statsRecord});
-          });
-        }
-      });
+    var playerPromise = Players.findOne(req.params.id).populate('team');
+
+    var statsPromise = playerPromise.then(function(playerRecord) {
+      if (typeof playerRecord === 'undefined' || playerRecord === null) {
+        res.notFound();
+      } else {
+        return Statistics.findOne({player: playerRecord.id, week: null, season: 1, team: null});
+      }
+    })
+    .catch(function(error) {
+      sails.log(error);
+      res.serverError();
+    });
+
+    Promise.all([playerPromise, statsPromise]).spread(function(playerRecord, statsRecord) {
+      res.view('players/view.jade', {player: playerRecord, stats: statsRecord});
+    })
+    .catch(function(error) {
+      sails.log(error);
+      res.serverError();
+    });
   }
 };
-
