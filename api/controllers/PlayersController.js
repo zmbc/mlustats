9912123700fd31ff19da1887f960fd497d4fcbca
Zeb Burke-Conte <zmbc@uw.edu>
2016-11-25 23:55:49 -0800

    Fix broken 404 logic

--- api/controllers/PlayersController.js
+++ api/controllers/PlayersController.js
@@ -9,12 +9,16 @@ var Promise = require('bluebird');
 
 module.exports = {
   view: function(req, res) {
-    var playerPromise = Players.findOne(req.params.id).populate('team').catch(res.notFound);
+    var playerPromise = Players.findOne(req.params.id).populate('team');
     var statsPromise = Players.statistics(req.params.id);
 
     Promise.all([playerPromise, statsPromise])
       .spread(function(player, stats) {
-        res.view({player: player, stats: stats, clientVars: {stats: stats}});
+        if (player == null || stats == null) {
+          res.notFound();
+        } else {
+          res.view({player: player, stats: stats, clientVars: {stats: stats}});
+        }
       })
       .catch(res.serverError);
   }
